17cdb7229cf205e858ed3326549453b3efe4c9e3
vigneshwara044 <vigneshwara044@gmail.com>
2019-02-18 18:25:38 +0530

    test/mData : improve mutable data tests - Include additional API calls in the MData tests
    - Add new test helpers for authenticator functions

--- safe-app/src/test/java/net/maidsafe/api/MDataTest.java
+++ safe-app/src/test/java/net/maidsafe/api/MDataTest.java
@@ -12,12 +12,22 @@ package net.maidsafe.api;
 import java.util.List;
 
 import net.maidsafe.api.model.EncryptKeyPair;
+import net.maidsafe.api.model.IpcRequest;
 import net.maidsafe.api.model.NativeHandle;
+import net.maidsafe.api.model.Request;
+import net.maidsafe.api.model.ShareMDataIpcRequest;
+import net.maidsafe.safe_app.AppExchangeInfo;
+import net.maidsafe.safe_app.AuthReq;
+import net.maidsafe.safe_app.ContainerPermissions;
 import net.maidsafe.safe_app.MDataEntry;
 import net.maidsafe.safe_app.MDataInfo;
 import net.maidsafe.safe_app.MDataKey;
 import net.maidsafe.safe_app.MDataValue;
+import net.maidsafe.safe_app.MetadataResponse;
 import net.maidsafe.safe_app.PermissionSet;
+import net.maidsafe.safe_app.ShareMData;
+import net.maidsafe.safe_app.ShareMDataReq;
+import net.maidsafe.safe_app.UserPermissionSet;
 import net.maidsafe.test.utils.Helper;
 import net.maidsafe.test.utils.SessionLoader;
 
@@ -25,14 +35,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
+
 public class MDataTest {
     static {
         SessionLoader.load();
     }
-
+    public static final String APP_ID = "net.maidsafe.java.test";
     static final int LENGTH = 10;
     static final int TYPE_TAG = 15001;
-
 
     private void publicMDataCrud(final MDataInfo mDataInfo) throws Exception {
         Session session = TestHelper.createSession();
@@ -43,28 +53,40 @@ public class MDataTest {
         permissionSet.setDelete(true);
         permissionSet.setManagePermission(true);
         NativeHandle permissionHandle = session.mDataPermission.newPermissionHandle().get();
-        session.mDataPermission.insert(permissionHandle, session.crypto.getAppPublicSignKey().get(),
-                permissionSet).get();
+        session.mDataPermission.insert(permissionHandle,
+                                       session.crypto.getAppPublicSignKey().get(),
+                                       permissionSet).get();
         NativeHandle entriesHandle = session.mDataEntries.newEntriesHandle().get();
-        session.mDataEntries.insert(entriesHandle, "someKey".getBytes(), "someValue".getBytes()).get();
+
+        String keyString = Helper.randomAlphaNumeric(LENGTH);
+        String valueString = Helper.randomAlphaNumeric(LENGTH);
+
+        session.mDataEntries.insert(entriesHandle, keyString.getBytes(),
+                                    valueString.getBytes()).get();
         session.mData.put(mDataInfo, permissionHandle, entriesHandle).get();
 
+        keyString = Helper.randomAlphaNumeric(LENGTH);
+        valueString = Helper.randomAlphaNumeric(LENGTH);
 
         NativeHandle actionHandle = session.mDataEntryAction.newEntryAction().get();
-        session.mDataEntryAction.insert(actionHandle, "someKey2".getBytes(),
-                "someValue2".getBytes()).get();
+        session.mDataEntryAction.insert(actionHandle, keyString.getBytes(),
+                valueString.getBytes()).get();
         session.mData.mutateEntries(mDataInfo, actionHandle).get();
 
         entriesHandle = session.mData.getEntriesHandle(mDataInfo).get();
         List<MDataEntry> entries = session.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(2, entries.size());
+        long length = session.mDataEntries.length(entriesHandle).get();
+        Assert.assertEquals(length, entries.size());
+
+        MDataValue mDataValue = session.mDataEntries.getValue(entriesHandle, keyString.getBytes()).get();
+        Assert.assertEquals(new String(mDataValue.getContent()), valueString);
 
         // Update
         actionHandle = session.mDataEntryAction.newEntryAction().get();
         MDataEntry entry = entries.get(0);
         byte[] updatedValue = Helper.randomAlphaNumeric(LENGTH).getBytes();
         session.mDataEntryAction.update(actionHandle, entry.getKey().getKey(), updatedValue,
-                entry.getValue().getEntryVersion() + 1).get();
+                                        entry.getValue().getEntryVersion() + 1).get();
         session.mData.mutateEntries(mDataInfo, actionHandle).get();
         entriesHandle = session.mData.getEntriesHandle(mDataInfo).get();
         entries = session.mDataEntries.listEntries(entriesHandle).get();
@@ -72,6 +94,7 @@ public class MDataTest {
         entry = entries.get(0);
         MDataValue value = session.mData.getValue(mDataInfo, entry.getKey().getKey()).get();
         Assert.assertEquals(new String(updatedValue), new String(value.getContent()));
+
         // Delete
         actionHandle = session.mDataEntryAction.newEntryAction().get();
         session.mDataEntryAction.delete(actionHandle, entry.getKey().getKey(),
@@ -80,8 +103,6 @@ public class MDataTest {
 
         entriesHandle = session.mData.getEntriesHandle(mDataInfo).get();
         entries = session.mDataEntries.listEntries(entriesHandle).get();
-
-
         Assert.assertEquals(entries.get(0).getValue().getContentLen(), 0);
 
         byte[] serialisedMData = session.mData.serialise(mDataInfo).get();
@@ -91,12 +112,13 @@ public class MDataTest {
         List<MDataValue> values = session.mData.getValues(mdInfo).get();
         Assert.assertEquals(keys.size(), values.size());
         Assert.assertEquals(new String(entries.get(0).getKey().getKey()),
-                new String(keys.get(0).getKey()));
+                            new String(keys.get(0).getKey()));
         Assert.assertEquals(new String(entries.get(0).getValue().getContent()),
-                new String(values.get(0).getContent()));
+                            new String(values.get(0).getContent()));
 
         PermissionSet permissions = session.mData.getPermissionForUser(
-                session.crypto.getAppPublicSignKey().get(), mdInfo).get();
+                                    session.crypto.getAppPublicSignKey().get(), mdInfo).get();
+
         Assert.assertTrue(permissions.getInsert());
         Assert.assertTrue(permissions.getUpdate());
         Assert.assertTrue(permissions.getRead());
@@ -109,11 +131,15 @@ public class MDataTest {
         permissionSet.setDelete(false);
         permissionHandle = session.mDataPermission.newPermissionHandle().get();
         session.mDataPermission.insert(permissionHandle, session.crypto.getAppPublicSignKey().get(),
-                permissionSet).get();
+                                        permissionSet).get();
         session.mData.setUserPermission(session.crypto.getAppPublicSignKey().get(), mdInfo,
-                permissionSet, session.mData.getVersion(mdInfo).get() + 1).get();
+                                        permissionSet,
+                                    session.mData.getVersion(mdInfo).get() + 1).get();
         long version = session.mData.getVersion(mdInfo).get();
         Assert.assertEquals(1, version);
+
+        length = session.mData.getSerialisedSize(mDataInfo).get();
+        Assert.assertTrue(length > 0);
     }
 
     private void privateMDataCrud(final MDataInfo mDataInfo) throws Exception {
@@ -125,22 +151,36 @@ public class MDataTest {
         permissionSet.setDelete(true);
         NativeHandle permissionHandle = session.mDataPermission.newPermissionHandle().get();
         session.mDataPermission.insert(permissionHandle, session.crypto.getAppPublicSignKey().get(),
-                permissionSet).get();
+                                        permissionSet).get();
         NativeHandle entriesHandle = session.mDataEntries.newEntriesHandle().get();
-        byte[] key = session.mData.encryptEntryKey(mDataInfo, "someKey".getBytes()).get();
-        byte[] value = session.mData.encryptEntryValue(mDataInfo, "someValue".getBytes()).get();
+
+        String keyString = Helper.randomAlphaNumeric(LENGTH);
+        String valueString = Helper.randomAlphaNumeric(LENGTH);
+
+        byte[] key = session.mData.encryptEntryKey(mDataInfo, keyString.getBytes()).get();
+        byte[] value = session.mData.encryptEntryValue(mDataInfo, valueString.getBytes()).get();
         session.mDataEntries.insert(entriesHandle, key, value).get();
         session.mData.put(mDataInfo, permissionHandle, entriesHandle).get();
 
+        keyString = Helper.randomAlphaNumeric(LENGTH);
+        valueString = Helper.randomAlphaNumeric(LENGTH);
+
         NativeHandle actionHandle = session.mDataEntryAction.newEntryAction().get();
-        key = session.mData.encryptEntryKey(mDataInfo, "someKey2".getBytes()).get();
-        value = session.mData.encryptEntryValue(mDataInfo, "someValue2".getBytes()).get();
+        key = session.mData.encryptEntryKey(mDataInfo, keyString.getBytes()).get();
+        value = session.mData.encryptEntryValue(mDataInfo, valueString.getBytes()).get();
         session.mDataEntryAction.insert(actionHandle, key, value).get();
         session.mData.mutateEntries(mDataInfo, actionHandle).get();
 
         entriesHandle = session.mData.getEntriesHandle(mDataInfo).get();
         List<MDataEntry> entries = session.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(2, entries.size());
+        long length = session.mDataEntries.length(entriesHandle).get();
+        Assert.assertEquals(length, entries.size());
+
+        key = session.mData.encryptEntryKey(mDataInfo, keyString.getBytes()).get();
+        MDataValue mDataValue = session.mDataEntries.getValue(entriesHandle, key).get();
+        byte[] decryptedValue = session.mData.decrypt(mDataInfo, mDataValue.getContent()).get();
+        Assert.assertEquals(new String(decryptedValue), valueString);
+
         // Update
         actionHandle = session.mDataEntryAction.newEntryAction().get();
         MDataEntry entry = entries.get(0);
@@ -151,7 +191,7 @@ public class MDataTest {
         session.mData.mutateEntries(mDataInfo, actionHandle).get();
 
         MDataValue entryValue = session.mData.getValue(mDataInfo, entry.getKey().getKey()).get();
-        byte[] decryptedValue = session.mData.decrypt(mDataInfo, entryValue.getContent()).get();
+        decryptedValue = session.mData.decrypt(mDataInfo, entryValue.getContent()).get();
         Assert.assertEquals(new String(updatedValue), new String(decryptedValue));
         // Delete
         entriesHandle = session.mData.getEntriesHandle(mDataInfo).get();
@@ -172,43 +212,168 @@ public class MDataTest {
         entriesHandle = session.mData.getEntriesHandle(mdInfo).get();
         entries = session.mDataEntries.listEntries(entriesHandle).get();
         Assert.assertEquals(2, entries.size());
+
+        length = session.mData.getSerialisedSize(mDataInfo).get();
+        Assert.assertTrue(length > 0);
     }
 
     @Test
     public void randomPublicMDataCRUDTest() throws Exception {
         Session session = TestHelper.createSession();
-        long tagType = TYPE_TAG;
-        MDataInfo mDataInfo = session.mData.getRandomPublicMData(tagType).get();
+        MDataInfo mDataInfo = session.mData.getRandomPublicMData(TYPE_TAG).get();
         publicMDataCrud(mDataInfo);
     }
 
     @Test
     public void publicMDataCRUDTest() throws Exception {
-        long tagType = TYPE_TAG;
         MDataInfo mDataInfo = new MDataInfo();
         mDataInfo.setName(Helper.randomAlphaNumeric(Constants.XOR_NAME_LENGTH).getBytes());
-        mDataInfo.setTypeTag(tagType);
+        mDataInfo.setTypeTag(TYPE_TAG);
         publicMDataCrud(mDataInfo);
     }
 
     @Test
     public void randomPrivateMDataCRUDTest() throws Exception {
         Session session = TestHelper.createSession();
-        long tagType = TYPE_TAG;
-        MDataInfo mDataInfo = session.mData.getRandomPrivateMData(tagType).get();
+        MDataInfo mDataInfo = session.mData.getRandomPrivateMData(TYPE_TAG).get();
         privateMDataCrud(mDataInfo);
     }
 
     @Test
     public void privateMDataCRUDTest() throws Exception {
         Session session = TestHelper.createSession();
-        long tagType = TYPE_TAG;
         EncryptKeyPair encryptKeyPair = session.crypto.generateEncryptKeyPair().get();
         byte[] nonce = session.crypto.generateNonce().get();
-        byte[] secretKey = session.crypto.getRawSecretEncryptKey(encryptKeyPair.getSecretEncryptKey())
-                .get();
-        MDataInfo mDataInfo = session.mData.getPrivateMData(Helper.randomAlphaNumeric(
-                Constants.XOR_NAME_LENGTH).getBytes(), tagType, secretKey, nonce).get();
+        byte[] secretKey = session.crypto.getRawSecretEncryptKey(encryptKeyPair.getSecretEncryptKey()).get();
+        MDataInfo mDataInfo = session.mData.getPrivateMData(
+                                        Helper.randomAlphaNumeric(Constants.XOR_NAME_LENGTH).getBytes(),
+                                        TYPE_TAG, secretKey, nonce).get();
         privateMDataCrud(mDataInfo);
+    }
+
+    @Test
+    public void mDataPermissionsTest() throws Exception {
+        Session session = TestHelper.createSession();
+        MDataInfo mDataInfo = session.mData.getRandomPublicMData(TYPE_TAG).get();
+
+        PermissionSet permissionSet = new PermissionSet();
+        permissionSet.setRead(true);
+        permissionSet.setInsert(true);
+        NativeHandle permissionHandle = session.mDataPermission.newPermissionHandle().get();
+        session.mDataPermission.insert(permissionHandle, Constants.USER_ANYONE,
+                                       permissionSet).get();
+
+        permissionSet.setUpdate(true);
+        permissionSet.setDelete(true);
+        permissionSet.setManagePermission(true);
+
+        NativeHandle appPublicSignKey = session.crypto.getAppPublicSignKey().get();
+        session.mDataPermission.insert(permissionHandle, appPublicSignKey,
+                                       permissionSet).get();
+
+        session.mData.put(mDataInfo, permissionHandle, Constants.MD_ENTRIES_EMPTY).get();
+
+        NativeHandle fetchedPermissionHandle = session.mData.getPermission(mDataInfo).get();
+        PermissionSet fetchedPermissionSet = session.mDataPermission.getPermissionForUser(
+                                                                            fetchedPermissionHandle,
+                                                                            appPublicSignKey).get();
+
+        Assert.assertTrue(fetchedPermissionSet.getRead());
+        Assert.assertTrue(fetchedPermissionSet.getInsert());
+        Assert.assertTrue(fetchedPermissionSet.getUpdate());
+        Assert.assertTrue(fetchedPermissionSet.getDelete());
+        Assert.assertTrue(fetchedPermissionSet.getManagePermission());
+
+        permissionSet.setDelete(false);
+        session.mData.setUserPermission(appPublicSignKey, mDataInfo, permissionSet,
+                                 session.mData.getVersion(mDataInfo).get() + 1).get();
+
+        PermissionSet newPermissionSet = session.mData.getPermissionForUser(appPublicSignKey,
+                                                                            mDataInfo).get();
+        Assert.assertEquals(newPermissionSet.getDelete(), false);
+
+        permissionHandle = session.mData.getPermission(mDataInfo).get();
+        long noOfPermissions = session.mDataPermission.getLength(permissionHandle).get();
+        List<UserPermissionSet> userPermissionSet = session.mDataPermission.listAll(
+                                                                            permissionHandle).get();
+        Assert.assertEquals(noOfPermissions, userPermissionSet.size());
+
+        session.mData.deleteUserPermission(Constants.USER_ANYONE, mDataInfo,
+                session.mData.getVersion(mDataInfo).get() + 1).get();
+        permissionHandle = session.mData.getPermission(mDataInfo).get();
+        userPermissionSet = session.mDataPermission.listAll(permissionHandle).get();
+        Assert.assertEquals(userPermissionSet.size(), 1);
+    }
+
+    @Test
+    public void sharedMutableDataTest() throws Exception {
+        Session appA, appB;
+        Authenticator authenticator = TestHelper.createAuthenticator();
+        ContainerPermissions[] permissions = new ContainerPermissions[1];
+        permissions[0] = new ContainerPermissions("_public", new PermissionSet(true, true,
+                true, true, true));
+
+        AuthReq authReq = new AuthReq(new AppExchangeInfo(APP_ID, "",
+                    Helper.randomAlphaNumeric(LENGTH), Helper.randomAlphaNumeric(LENGTH)),
+                    true, permissions, 1, 0);
+        appA = TestHelper.handleAuthReq(authenticator, authReq);
+
+        MDataInfo mDataInfo = new MDataInfo();
+        mDataInfo.setName(Helper.randomAlphaNumeric(Constants.XOR_NAME_LENGTH).getBytes());
+        mDataInfo.setTypeTag(TYPE_TAG);
+
+        String keyString = Helper.randomAlphaNumeric(LENGTH);
+        String valueString = Helper.randomAlphaNumeric(LENGTH);
+        NativeHandle entriesHandle = appA.mDataEntries.newEntriesHandle().get();
+        byte[] key = appA.mData.encryptEntryKey(mDataInfo, keyString.getBytes()).get();
+        byte[] value = appA.mData.encryptEntryValue(mDataInfo, valueString.getBytes()).get();
+        appA.mDataEntries.insert(entriesHandle, key, value).get();
+
+        MetadataResponse metadataResponse = new MetadataResponse();
+        metadataResponse.setName("Test MData");
+        metadataResponse.setDescription("MData for testing");
+        metadataResponse.setXorName(mDataInfo.getName());
+        metadataResponse.setTypeTag(mDataInfo.getTypeTag());
+        byte[] encodedResponse = appA.mData.encodeMetadata(metadataResponse).get();
+
+        appA.mDataEntries.insert(entriesHandle, Constants.MD_METADATA_KEY.getBytes(), encodedResponse).get();
+        appA.mData.put(mDataInfo, Constants.MD_PERMISSION_EMPTY, entriesHandle).get();
+
+        authReq.getApp().setId("net.maidsafe.app.two");
+        appB = TestHelper.handleAuthReq(authenticator, authReq);
+        Request shareMDataIpcRequest;
+        AppExchangeInfo appExchangeInfo = new AppExchangeInfo("net.maidsafe.app.two",
+                                                "", "App two", "Maidsafe.net");
+        ShareMData[] shareMData = new ShareMData[1];
+        shareMData[0] =  new ShareMData(mDataInfo.getTypeTag(),
+                                mDataInfo.getName(), new PermissionSet(true, true, true,
+                                true, false));
+
+        ShareMDataReq shareMDataReq = new ShareMDataReq(appExchangeInfo, shareMData, 1, 1);
+        shareMDataIpcRequest =  Session.getShareMutableDataRequest(shareMDataReq).get();
+
+        IpcRequest decodedReq = authenticator.decodeIpcMessage(shareMDataIpcRequest.getUri()).get();
+        ShareMDataIpcRequest shareMDataIpcReq = (ShareMDataIpcRequest) decodedReq;
+
+        Assert.assertEquals(shareMDataIpcReq.getMetadataResponse().length, 1);
+        Assert.assertEquals(shareMDataIpcReq.getMetadataResponse()[0].getDescription(),
+                            metadataResponse.getDescription());
+        Assert.assertEquals(shareMDataIpcReq.getMetadataResponse()[0].getName(),
+                            metadataResponse.getName());
+        Assert.assertEquals(new String(shareMDataIpcReq.getMetadataResponse()[0].getXorName()),
+                            new String(metadataResponse.getXorName()));
+        Assert.assertEquals(shareMDataIpcReq.getMetadataResponse()[0].getTypeTag(),
+                            metadataResponse.getTypeTag());
+
+        authenticator.encodeShareMDataResponse(shareMDataIpcReq, true).get();
+
+        NativeHandle actionHandle = appB.mDataEntryAction.newEntryAction().get();
+        keyString = Helper.randomAlphaNumeric(LENGTH);
+        valueString = Helper.randomAlphaNumeric(LENGTH);
+        appB.mDataEntryAction.insert(actionHandle, keyString.getBytes(), valueString.getBytes()).get();
+        appB.mData.mutateEntries(mDataInfo, actionHandle).get();
+
+        MDataValue mDataValue = appB.mData.getValue(mDataInfo, keyString.getBytes()).get();
+        Assert.assertEquals(valueString, new String(mDataValue.getContent()));
     }
 }
